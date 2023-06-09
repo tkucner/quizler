1c37dc9b19527d9d8c0238e3d672673ad95ea1c2
Kucner Tomasz <tomasz.kucner@aalto.fi>
2023-06-09 07:34:45 +0300

    added automatic group computation

--- quizler.py
+++ quizler.py
@@ -2,6 +2,50 @@ import os
 import sys
 import jsonschema
 import json
+import csv
+
+def remove_letters(input_string):
+    input_string=str(input_string)
+    return ''.join(c for c in input_string if not c.isalpha())
+
+def fun_add(factor,number):
+    return number+factor
+
+def fun_substract(factor,number):
+    return number-factor
+
+def fun_multiply(factor,number):
+    return number*factor
+
+def fun_sum(_,number):
+    total = 0
+    for digit in str(number):
+        total += int(digit)
+    return total
+
+def number_to_group(number,group_count):
+    hex_digits = '0123456789ABCDEF'
+    remainder = number % group_count
+    hex_digit = hex_digits[remainder]
+    return hex_digit
+
+def fun_take(factor,number):
+    return int(str(number)[-factor:])
+
+def compute_group(number,process,group_count):
+    function_dictionary={
+        "add":fun_add,
+        "substract":fun_substract,
+        "multiply":fun_multiply,
+        "sum":fun_sum,
+        "take":fun_take
+    }
+    number=remove_letters(number)
+    for fun,val in process:
+        number=function_dictionary[fun](val,number)
+    return number_to_group(number,group_count)
+
+
 
 def find_schema(extension="schema.json"):
     for root, dirs, files in os.walk('.'):
@@ -43,5 +87,19 @@ if len(sys.argv) == 3:
     json_file=sys.argv[2]
 
 json_data=validate_json(json_file,schema_file)
-print(json_data)
+students=[]
+with open(json_data['students_list'], 'r',encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter=";")
+
+    # Read the contents of the CSV file into a list
+    rows = []
+    for row in csv_reader:
+        rows.append(row)
+
+# Perform the mathematical operation on the third column of each row
+for i in range(len(rows)):
+    value = rows[i][2]
+
+    result = compute_group(value,json_data['group_computation'],json_data['group_count'])
+    rows[i].append(str(result))
 
